b0461843624adc3054f56d62d29d06aa0d4b638d
Dixon Lamb <dixonlamb@gmail.com>
2016-04-29 19:25:28 +0000

    Initial argument parse mockup, DOES NOT WORK

--- src/argumentParse.c
+++ src/argumentParse.c
@@ -5,9 +5,49 @@
     Returns argument count.
 */
 int parse (char** input) {
-    int argc = 0;
+    int argument_count = 0;
+    int quotes_count = 0;
+    char* idx;
+    char** argument = input;
     
-    fprintf(stdout, "'%s'\n", *input);
+    while (**argument != 0) {
+        
+        // Leading whitespaces
+        while (**argument == ' ' || **argument == '\n') {
+            **argument++ = 0;
+        }
+        
+        // Found argument
+        if (**argument != 0) {
+            argument_count++;
+            idx = argument;
+        }
+        
+        while (**argument != 0 && **argument != ' ' && **argument != '\n') {
+            if(**argument == '\"') {
+                quotes_count++;
+                argument++;
+                
+                **(argument - quotes_count) = **argument;
+                while (**argument != '\"') {
+                    argument++;
+                    *(argument - quotes_count);
+                }
+                quotes_count++;
+                
+                argument++;
+                **(argument - quotes_count) = **argument;
+                **(argument - (quotes_count - 1)) = **argument;
+            } else {
+                argument++;
+                **(argument - quotes_count) = **argument;
+            }
+        }
+        quotes_count = 0;
+        *argument++ = idx;
+    }
     
-    return argc;
+    *argument = 0;
+    
+    return argument_count;
 }
